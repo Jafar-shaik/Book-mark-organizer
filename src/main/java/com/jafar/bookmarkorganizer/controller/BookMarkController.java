8726ae8276053962e4459ff8134ad9060e609653
Jafar-shaik <skjafar4944@gmail.com>
2025-05-19 12:39:57 +0530

    Adding ResponseEntity for each method in controller

--- src/main/java/com/jafar/bookmarkorganizer/controller/BookMarkController.java
+++ src/main/java/com/jafar/bookmarkorganizer/controller/BookMarkController.java
@@ -4,9 +4,10 @@ package com.jafar.bookmarkorganizer.controller;
 import com.jafar.bookmarkorganizer.entity.BookMark;
 import com.jafar.bookmarkorganizer.service.BookMarkService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.awt.print.Book;
 import java.util.List;
 
 @RestController
@@ -17,26 +18,43 @@ public class BookMarkController {
     private BookMarkService bookMarkService;
 
     @PostMapping("create")
-    public BookMark createBookMark(@RequestBody BookMark bookMark){
-        return bookMarkService.createBookMark(bookMark);
+    public ResponseEntity<?> createBookMark(@RequestBody BookMark myBookMark) {
+        try {
+            BookMark bookMark = bookMarkService.createBookMark(myBookMark);
+            return new ResponseEntity<>(bookMark, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+
     }
 
     @GetMapping
-    public List<BookMark> getAllBookMArks(){
-        return bookMarkService.getAllBookMarks();
+    public ResponseEntity<?> getAllBookMArks() {
+        List<BookMark> list = bookMarkService.getAllBookMarks();
+        if (list != null && !list.isEmpty())
+            return new ResponseEntity<>(list, HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @DeleteMapping("/delete/{id}")
-    public void deleteBookMark(@PathVariable String id){
-         bookMarkService.deleteBookMark(id);
+    public ResponseEntity<?> deleteBookMark(@PathVariable String id) {
+        bookMarkService.deleteBookMark(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @GetMapping("/search")
-    public List<BookMark> search(@RequestParam (required = false) String tag, @RequestParam (required = false) String keyword){
-        if(tag!=null) return bookMarkService.findByTagContaining(tag);
-        if(keyword!=null) return bookMarkService.searchByKeyWordContaining(keyword);
+    public ResponseEntity<?> search(@RequestParam(required = false) String tag, @RequestParam(required = false) String keyword) {
+        if (tag != null) {
+            List<BookMark> byTagContaining = bookMarkService.findByTagContaining(tag);
+            return new ResponseEntity<>(byTagContaining,HttpStatus.FOUND);
+        }
+        if (keyword != null) {
+            List<BookMark> bookMarks = bookMarkService.searchByKeyWordContaining(keyword);
+            return new ResponseEntity<>(bookMarks,HttpStatus.FOUND);
+        }
 
-        return bookMarkService.getAllBookMarks();
+        List<BookMark> list = bookMarkService.getAllBookMarks();
+        return new ResponseEntity<>(list,HttpStatus.FOUND);
 
     }
 
